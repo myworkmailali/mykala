c08965c2d8d618e2dc350c4e99d50b21aba45636
user1 <myworkmailali@gmail.com>
2025-11-02 14:44:44 +0330

    Session_02_Models

--- mykala/settings.py
+++ mykala/settings.py
@@ -111,3 +111,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL='/static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL= '/media/'
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import datetime
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=0, max_digits=10,default=0)
+    description = models.CharField(max_length=500,default='', blank=True,null=True)
+    picture=models.ImageField(upload_to='upload/product')
+    def __str__(self):
+        return f'{self.name}'
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=500,default='',blank=True)
+    phone_number = models.CharField(max_length=20,blank=True,null=True)
+    date = models.DateField(default=datetime.date.today)
+    status = models.BooleanField(default=False)
+    def __str__(self):
+        return f'{self.product}'
