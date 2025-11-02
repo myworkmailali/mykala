cbac71c68f3885cb156cbed804c4c6be92cdb660
user1 <myworkmailali@gmail.com>
2025-11-02 15:32:39 +0330

    Session_03_Bootstrap Template

--- mykala/settings.py
+++ mykala/settings.py
@@ -117,3 +117,9 @@ STATIC_URL='/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL= '/media/'
+
+
+
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
--- shop/models.py
+++ shop/models.py
@@ -15,10 +15,17 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
     price = models.DecimalField(decimal_places=0, max_digits=10,default=0)
     description = models.CharField(max_length=500,default='', blank=True,null=True)
     picture=models.ImageField(upload_to='upload/product')
+
+#    SIZE_CHOICES = (
+#        ('m', '32'),
+#        ('l', '42'),
+#        ('xl', '52'),
+#    )
+#    size = models.CharField(max_length=4,choices=SIZE_CHOICES,default='32')
     def __str__(self):
         return f'{self.name}'
 
