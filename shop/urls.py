2f5fac09a6b1891dd31f552cd21b5f412f1d7f5c
user1 <myworkmailali@gmail.com>
2025-11-06 12:56:21 +0330

    Session_07_Loggin/loggout

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path,include
-from .views import index,about
-
+from .views import index, about, login_user,logout_user
 
 urlpatterns = [
 
     path('', index,name='home'),
     path('about/',about,name='about'),
+    path('login/',login_user,name='login'),
+    path('logout/',logout_user,name='logout')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+
+from django.shortcuts import render,redirect
 from .models import Product
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+
 
 def index(request):
     all_products = Product.objects.all()
@@ -7,3 +12,21 @@ def index(request):
 
 def about(request):
     return render(request,'about.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, 'شما با موفقیت وارد سامانه شدید')
+            return redirect('home')
+        else:
+            messages.error(request, 'نام کاربری یا کلمه عبور نادرست است')
+    return render(request,'login.html' )
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,'شما با موفقیت از سامانه خارج شدید')
+    return redirect('home')
