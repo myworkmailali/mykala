5b46244e497306dad04304c734002c75216b87d4
user1 <myworkmailali@gmail.com>
2025-11-08 14:51:34 +0330

    Session_11_CartCreate

--- mykala/urls.py
+++ mykala/urls.py
@@ -9,4 +9,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
+    path('cart/', include('cart.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
