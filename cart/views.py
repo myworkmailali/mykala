cb537f589ff4470aca092813af49fb41df51b5dc
user1 <myworkmailali@gmail.com>
2025-11-08 21:54:22 +0330

    Session_13_Session_Add_Product

--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from http.client import responses
+
+from django.shortcuts import render,get_object_or_404
+from .cart import Cart
+from shop.models import Product
+from django.http import JsonResponse
 
 def cart_summary(request):
     return render(request,'cart_summary.html',{})
 
 def cart_delete(request):
     pass
+
 def cart_add(request):
-    pass
+    cart= Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id =int( request.POST.get('product_id'))
+        product = get_object_or_404(Product,id=product_id)
+        cart.add(product=product)
+        response=JsonResponse({'product name':product.name})
+        return response
+
 def cart_update(request):
     pass
