b1978d76c33c495c3d242ef38ebe729e3fcc68ea
user1 <myworkmailali@gmail.com>
2025-11-11 08:07:51 +0330

    Session_34_Order_Confirm

--- payment/urls.py
+++ payment/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 
     path('payment_success/', views.payment_success,name='payment_success'),
     path('checkout/', views.checkout,name='checkout'),
+    path('order_confirm/', views.order_confirm,name='order_confirm'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -18,7 +18,23 @@ def checkout(request):
     if request.user.is_authenticated:
         current_user=ShippingAddress.objects.get(user__id=request.user.id)
         shipping_form=ShippingAddressForm(request.POST or None,instance=current_user)
-    if shipping_form.is_valid():
-        shipping_form.save()
-        messages.success(request, 'آدرس ارسال محصول با موفقیت ثبت شد')
+#    if shipping_form.is_valid():
+#        shipping_form.save()
+#        messages.success(request, 'آدرس ارسال محصول با موفقیت ثبت شد')
     return render(request,'payment/checkout.html',{'products': products, 'quantities': quantities, 'total_ordering': total_ordering,'shipping_form':shipping_form})
+
+
+def order_confirm(request):
+    cart = Cart(request)
+    products = cart.get_products()
+    quantities = cart.get_quantity()
+    total_ordering = cart.get_total()
+    shipping_info=request.POST
+    request.session['shipping_info']=shipping_info
+    if request.method == 'POST':
+       pass
+    else:
+       messages.success(request,'شما دسترسی به این صفحه را ندارید')
+       return redirect('home')
+    return render(request,'payment/order_confirm.html',{'products': products, 'quantities': quantities, 'total_ordering': total_ordering,'shipping_info':shipping_info})
+
